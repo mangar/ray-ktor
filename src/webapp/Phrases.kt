734d92b3fb45ac01b777bf3a03522d91a12c1d9f
Marcio Garcia <marcio.garcia@rga.com>
2019-10-04 16:41:51 -0300

    POST to create the phrases

--- src/webapp/Phrases.kt
+++ src/webapp/Phrases.kt
@@ -5,8 +5,10 @@ import com.rw.model.*
 import io.ktor.application.*
 import io.ktor.auth.*
 import io.ktor.freemarker.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import java.lang.IllegalArgumentException
 import kotlin.text.get
 
 
@@ -22,6 +24,14 @@ fun Route.phrases(db: Repository) {
         mapOf("phrases" to phrases,
         "userName" to user.displayName))
       )
+    }
+    post(PHRASES) {
+      val params = call.receiveParameters()
+      val emoji = params["emoji"] ?: throw IllegalArgumentException("Missing parameter: emoji")
+      val phrase = params["phrase"] ?: throw IllegalArgumentException("Missing parameter: phrase")
+
+      db.add((EmojiPhrase(emoji, phrase)))
+      call.respondRedirect(PHRASES)
     }
   }
 
